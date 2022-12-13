291d4973aa0c156f75b75a6a62f47f4acd480e3c
Nick <nicolastiennguyen@gmail.com>
2022-12-12 21:24:58 -0800

    removed versionKey option

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -13,8 +13,7 @@ const BookSchema: Schema = new Schema(
         author: { type: Schema.Types.ObjectId, required: true, ref: 'Author' }
     },
     {
-        timestamps: true,
-        versionKey: true
+        timestamps: true
     }
 );
 

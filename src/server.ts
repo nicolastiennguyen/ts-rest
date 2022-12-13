8173cfed988031ff52e9b05e16e46b396726081b
Nick <nicolastiennguyen@gmail.com>
2022-12-12 16:36:35 -0800

    server setup

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,63 @@ import Logging from './library/Logging';
 const router = express();
 
 /* Connect to Mongo */
+mongoose.set('strictQuery', true);
 mongoose
     .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
     .then(() => {
         Logging.info('Connected to mongoDB.');
+        StartServer();
     })
     .catch((error) => {
         Logging.error('Unable to connect: ');
         Logging.error(error);
     });
+
+/* Only start the server if Mongo connects */
+const StartServer = () => {
+    router.use((req, res, next) => {
+        /* log the request */
+        Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            /* log the response*/
+            Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+        });
+
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    /** Rules of our API */
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+
+    /* Routes */
+
+    /* Healthcheck to make sure API is working */
+    router.get('/ping', (req, res, next) => {
+        res.status(200).json({ message: 'pong' });
+    });
+
+    /** Error handling */
+    router.use((req, res, next) => {
+        const error = new Error('not found');
+        Logging.error(error);
+        return res.status(404).json({ message: error.message });
+    });
+
+    http.createServer(router).listen(config.server.port, () => {
+        Logging.info(`Server is running on port ${config.server.port}.`);
+    });
+};
